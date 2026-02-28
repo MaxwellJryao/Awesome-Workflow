1d1e6d1eeadeec34a23e1475322750bbca273883
Jiarui Yao <maxwell.jryao@gmail.com>
2026-02-28 21:22:24 +0800

    refactor setup script to improve installation commands for ohmyzsh and uv

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,5 +1,5 @@
 # install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -10,5 +10,7 @@ mv ~/.zshrc ~/.zshrc.bak
 cp example.sh ~/.zshrc
 
 # install uv
-curl -LsSf https://astral.sh/uv/install.sh | sh
+curl -LsSf https://astral.sh/uv/install.sh -o /tmp/uv-install.sh
+sh /tmp/uv-install.sh
+rm -f /tmp/uv-install.sh
 source $HOME/.local/bin/env
